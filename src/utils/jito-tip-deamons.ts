2949b62a7405ee9585c48f5f42eec60900e5cfc5
Spiry <spirymedia@gmail.com>
2024-10-18 23:00:51 +0300

    Update jito-tip-deamons.ts

--- src/utils/jito-tip-deamons.ts
+++ src/utils/jito-tip-deamons.ts
@@ -72,7 +72,7 @@ async function getAverageJitoTip() {
   try {
     const response = await axios({
       method: "get",
-      url: "http://bundles-api-rest.jito.wtf/api/v1/bundles/tip_floor",
+      url: "https://explorer.jito.wtf/bundle-explorer?sortBy=Tip",
       params: {
         limit: 200,
         sort: "Time",
