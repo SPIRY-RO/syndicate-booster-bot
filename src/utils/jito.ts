212c851fd086b993033d0cbcae00736daefcb700
Spiry <spirymedia@gmail.com>
2024-10-19 00:52:51 +0300

    Update jito.ts

--- src/utils/jito.ts
+++ src/utils/jito.ts
@@ -23,7 +23,7 @@ export async function makeAndSendJitoBundle(
   txs: solana.VersionedTransaction[], keypair: solana.Keypair, tipOverride_inLamps?: number,
 ): Promise<boolean> {
   if (!tipOverride_inLamps)
-    tipOverride_inLamps = Math.min(1250000);
+    tipOverride_inLamps = 1250000;
 
   try {
     //const txNum = Math.ceil(txs.length / 3);
