05fddda8f1b44e8932328ce8e8bc937a11b38335
Spiry <spirymedia@gmail.com>
2024-10-21 22:01:24 +0300

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -53,7 +53,7 @@ export const DEFAULT_NUM_OF_CU_PER_TX = 200000; // compute units per transaction
 export const RENT_HOUR_TO_PRICE_MAP: any = {
   // hours : amount of SOL
   "1": 0.5,
-  "3": 1.5,
+  "3": 3,
   "6": 3,
   "12": 5,
   "24": 8,
