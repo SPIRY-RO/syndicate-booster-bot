72677d02953444a27580d5e103d8156c36e2f5b8
Spiry <spirymedia@gmail.com>
2025-02-10 22:46:49 +0200

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -25,14 +25,14 @@ export const POOL_UPDATE_INTERVAL = 1200 * 1000;
 export const POOL_DATA_LARGE_URL = "https://api.raydium.io/v2/sdk/liquidity/mainnet.json";
 
 export const JUPITER_API_URL = envConf.JUPITER_API_URL; // self-hosted
-//export const JUPITER_API_URL = 'https://quote-api.jup.ag/v6'; // public
 
 export const JITO_BUNDLE_CHECK_TIMEOUT = 25 * 1000;
 export const JITO_STATUS_CHECK_INTERVAL = 2 * 1000; // check new bundle status this often
 
 export const BALANCE_CHANGE_CHECK_TIMEOUT = 30 * 1000;
 export const JITO_BUNDLE_TIMEOUT = 30 * 1000;
-//export const JITO_MAX_BUNDLES_PER_SEC_RATE_LIMIT = 5; // for regular keys
+
+
 export const JITO_MAX_BUNDLES_PER_SEC_RATE_LIMIT = 50; // for our special key
 export const JITO_MAX_BUNDLE_IDS_PER_STATUS_CHECK = 5;
 
